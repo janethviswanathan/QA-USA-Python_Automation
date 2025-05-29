d30943b351541690376dfd5529b7a3f0ad215c71
janeth.viswanathan <janeth.viswanathan@gmail.com>
2025-05-28 21:35:57 -0400

    Initial Commit after repo fix

--- data.py
+++ data.py
@@ -1,1 +1,12 @@
-# File with constants
+# data.py
+
+# Constant for the Urban Routes URL (used by the start server button)
+URBAN_ROUTES_URL = 'https://cnt-49d91bd0-956b-4919-bf46-c7ad7ee1029f.containerhub.tripleten-services.com'
+
+# Test data constants
+ADDRESS_FROM = 'East 2nd Street, 601'
+ADDRESS_TO = '1300 1st St'
+PHONE_NUMBER = '+1 123 123 12 12'
+CARD_NUMBER = '1234 5678 9100'
+CARD_CODE = '1111'
+MESSAGE_FOR_DRIVER = 'Stop at the juice bar, please'
--- main.py
+++ main.py
@@ -1,2 +1,68 @@
+# main.py
+
+from selenium.webdriver.common.by import By
+from data import *
+from helpers import *
+from selenium import webdriver
+from selenium.webdriver.support.wait import WebDriverWait
+
+# driver.implicitly_wait(3)
+# driver.find_element(By.TAG_NAME, "button")
+# driver.find_element(By.TAG_NAME, "div")
+# driver.find_element(By.TAG_NAME, "button")
+# driver.find_element(By.TAG_NAME, "imput")
+# driver.find_element(By.ID, "from").send_keys("East 2nd Street, 601")
+# driver.find_element(By.ID, "to").send_keys("1300 1st St")
+# driver.find_element(By.ID, "from").clear()
+# driver.find_element(By.CLASS_NAME, "logo-disclaimer").text
 
 class TestUrbanRoutes:
+
+    @classmethod
+    def setup_class(cls):
+      if is_url_reachable(URBAN_ROUTES_URL):
+          print("Connected to the Urban Routes server")
+      else:
+          print("Cannot connect to Urban Routes. Check the server is on and still running")
+
+    def test_set_route(self):
+        # Add in S8
+        print("function created for set route")
+        pass
+
+    def test_select_plan(self):
+        # Add in S8
+        print("function created for select plan")
+        pass
+
+    def test_fill_phone_number(self):
+        # Add in S8
+        print("function created for fill phone number")
+        pass
+
+    def test_fill_card(self):
+        # Add in S8
+        print("function created for fill card")
+        pass
+
+    def test_comment_for_driver(self):
+        # Add in S8
+        print("function created for comment for driver")
+        pass
+
+    def test_order_blanket_and_handkerchiefs(self):
+        # Add in S8
+        print("function created for order blanket and handkerchiefs")
+        pass
+
+    def test_order_2_ice_creams(self):
+        # Add in S8
+        print("function created for order 2 ice creams")
+        for _ in range(2):
+            # Add in S8
+            pass
+
+    def test_car_search_model_appears(self):
+        # Add in S8
+        print("function created for car search model appears")
+        pass
